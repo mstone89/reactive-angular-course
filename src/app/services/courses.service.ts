78d55a83e8a86e1846be7c7a1017c5c8223037d3
Molly Stone <molly.stone89@gmail.com>
2020-07-17 22:04:58 -0400

    Add share replay to http get observable

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,4 +1,4 @@
-import { map } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { Course } from './../model/course';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -14,7 +14,8 @@ export class CoursesService {
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>('/api/courses')
       .pipe(
-        map(response => response['payload'])
+        map(response => response['payload']),
+        shareReplay()
       );
   }
 }
